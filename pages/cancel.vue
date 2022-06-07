3af96489336b6c0e2a6c08d3ae0bf4733aa96394
Po-Wei Chen <erase2004@gmail.com>
2022-06-07 23:06:45 +0800

    feat: add confirm page and cancel page

---
+++ pages/cancel.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl">Payment Cancelled</h1>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'single'
+})
+</script>
+
+<style scoped></style>
---
+++ pages/confirm.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl">Payment Confirmed</h1>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'single'
+})
+</script>
+
+<style scoped></style>
