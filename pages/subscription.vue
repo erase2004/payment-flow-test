1cf4aa5cfa5f6621e018ad4cbced763ebe6fe1cf
Po-Wei Chen <erase2004@gmail.com>
2022-06-07 23:07:36 +0800

    feat: add LINE Pay subscription page and function

---
+++ pages/subscription.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button
+      class="my-4 p-2 border-2 rounded-md bg-[#00b700] text-white hover:bg-[#005700] disabled:opacity-25 disabled:cursor-not-allowed"
+      @click.stop.prevent="callPayment"
+      :disabled="isProcessing"
+    >
+      Pay with LINE Pay (Subscription)
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'tab'
+})
+
+const isProcessing = ref(false)
+
+async function callPayment() {
+  try {
+    isProcessing.value = true
+
+    const result = await $fetch('/api/subscription', { method: 'POST' })
+    const targetUrl = result?.body?.info?.paymentUrl?.web
+
+    // redirect to payment page
+    window.location.href = targetUrl
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isProcessing.value = false
+  }
+}
+
+</script>
+<style scoped></style>
