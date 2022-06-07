8e4f60f1bc8a5bfe9c758a15098b9d646abaf9ee
Po-Wei Chen <erase2004@gmail.com>
2022-06-07 23:08:39 +0800

    feat: add subscription confirm page

---
+++ pages/subscription-confirm.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1 class="text-3xl">Subscription Confirmed</h1>
+  <div>
+    <button
+      class="my-4 p-2 border-2 rounded-md bg-[#00b700] text-white hover:bg-[#005700] disabled:opacity-25 disabled:cursor-not-allowed"
+      @click.stop.prevent="callPayment"
+      :disabled="isProcessing"
+    >
+      Request Next Payment
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'single'
+})
+
+const { query } = useRoute()
+
+const isProcessing = ref(false)
+
+async function callPayment() {
+  try {
+    isProcessing.value = true
+
+    const result = await $fetch('/api/preapproved', { method: 'POST', body: query })
+
+    alert(JSON.stringify(result))
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isProcessing.value = false
+  }
+}
+
+</script>
+
+<style scoped></style>
