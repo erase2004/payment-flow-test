adfe69abac64abe6332e21776da7ee61d35758ea
Tsuki Akiba <erase2004@gmail.com>
2022-06-08 16:17:26 +0800

    refactor: get value from cookie instead of url parameter

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1 class="text-3xl">Payment Confirmed</h1>
+  <div>
+    <h1 class="text-3xl">Payment Confirmed</h1>
+      <pre class="mx-auto my-4 text-left w-1/2 whitespace-pre-wrap break-words">
+<h2 class="text-xl">Repsonse Data</h2>
+<code class="text-xs">
+{{ confirmInfo }}
+</code>
+  </pre>
+    <NuxtLink
+      to="/check-payment"
+      class="hover:opacity-50"
+    >
+      Check Payment Status
+    </NuxtLink>
+  </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
   layout: 'single'
 })
+
+const confirmInfo = useCookie('confirm')
+
 </script>
 
 <style scoped></style>
--- pages/subscription-confirm.vue
+++ pages/subscription-confirm.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1 class="text-3xl">Subscription Confirmed</h1>
   <div>
+    <h1 class="text-3xl">Subscription Confirmed</h1>
     <button
       class="my-4 p-2 border-2 rounded-md bg-[#00b700] text-white hover:bg-[#005700] disabled:opacity-25 disabled:cursor-not-allowed"
       @click.stop.prevent="callPayment"
@@ -8,6 +8,12 @@
     >
       Request Next Payment
     </button>
+    <pre class="mx-auto my-4 text-left w-1/2 whitespace-pre-wrap break-words">
+<h2 class="text-xl">Repsonse Data</h2>
+<code class="text-xs">
+{{ responseInfo }}
+</code>
+    </pre>
   </div>
 </template>
 
@@ -18,15 +24,14 @@ definePageMeta({
 
 const regKey = useCookie('regKey')
 const isProcessing = ref(false)
+let responseInfo = reactive({})
 
 async function callPayment() {
   try {
     isProcessing.value = true
     const result = await $fetch('/api/preapproved', { method: 'POST', body: { regKey: regKey.value} })
 
-    const result = await $fetch('/api/preapproved', { method: 'POST', body: query })
-
-    alert(JSON.stringify(result))
+    responseInfo = Object.assign(responseInfo, result)
   } catch (error) {
     console.error(error)
   } finally {
