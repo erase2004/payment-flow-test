797e2357408bddf9a1da9b7ccecf6de48fe311eb
Tsuki Akiba <erase2004@gmail.com>
2022-09-01 18:28:28 +0800

    refactor: add `LINE_PAY_CLIENT_MODE`

--- config.ts
+++ config.ts
@@ -5,3 +5,4 @@ export const SITE_URL = env.SITE_URL || 'http://localhost:3000'
 // LINE Pay API Client
 export const LINE_PAY_CHANNEL_ID = env.LINE_PAY_CHANNEL_ID || ''
 export const LINE_PAY_CHANNEL_KEY = env.LINE_PAY_CHANNEL_KEY || ''
+export const LINE_PAY_CLIENT_MODE = ['development', 'production'].includes(env.LINE_PAY_CLIENT_MODE) ? env.LINE_PAY_CLIENT_MODE : 'development'
--- server/utils/linePayClient.ts
+++ server/utils/linePayClient.ts
@@ -1,10 +1,10 @@
 import { createLinePayClient } from 'line-pay-merchant'
-import { LINE_PAY_CHANNEL_ID, LINE_PAY_CHANNEL_KEY  } from '~~/config'
+import { LINE_PAY_CHANNEL_ID, LINE_PAY_CHANNEL_KEY, LINE_PAY_CLIENT_MODE } from '~~/config'
 
 const linePayClient = createLinePayClient({
   channelId: LINE_PAY_CHANNEL_ID,
   channelSecretKey: LINE_PAY_CHANNEL_KEY,
-  env: 'development'
+  env: LINE_PAY_CLIENT_MODE as 'development'|'production',
 })
 
 export default linePayClient
