720c08be0887c1eff99e63e7a5841368addf05c1
Tsuki Akiba <erase2004@gmail.com>
2023-06-08 14:36:40 +0800

    refactor: add error handle to `/api/confirm`

--- server/api/confirm.get.ts
+++ server/api/confirm.get.ts
@@ -4,19 +4,24 @@ import { sendRedirect, setCookie } from "h3"
 export default defineEventHandler(async (event) => {
   const query = useQuery(event)
 
-  const res = await linePayClient.confirm.send({
-    transactionId: query.transactionId,
-    body: {
-      currency: 'TWD',
-      amount: 1
+  try {
+    const res = await linePayClient.confirm.send({
+      transactionId: query.transactionId,
+      body: {
+        currency: 'TWD',
+        amount: 1
+      }
+    })
+
+    if (res.body.info.regKey) {
+      setCookie(event, 'subscription-confirm', JSON.stringify(res.body))
+      return sendRedirect(event, "/subscription-confirm")
     }
-  })
 
-  if (res.body.info.regKey) {
-    setCookie(event, 'subscription-confirm', JSON.stringify(res.body))
-    return sendRedirect(event, "/subscription-confirm")
+    setCookie(event, 'confirm', JSON.stringify(res.body))
+    return sendRedirect(event, '/confirm')
+  } catch (err) {
+    console.error(err);
+    return sendRedirect(event, "/confirm")
   }
-
-  setCookie(event, 'confirm', JSON.stringify(res.body))
-  return sendRedirect(event, '/confirm')
 })
